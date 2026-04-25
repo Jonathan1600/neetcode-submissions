e857ab41452d10df74af1bf28f0e9634f5246114
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-04-25 01:59:00 -0700

    Add: linked-list-cycle-detection - submission-6

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-6.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        if (!head || !head.next || !head.next.next) {
+            return false
+        }
+        let l1 = head
+        let l2 = head.next
+        while (l2 != l1) {
+            if(!l2 || !l2.next || !l1.next) {
+                return false
+            }
+            l1 = l1.next
+            l2 = l2.next.next
+        }
+        return true
+    }
+}
