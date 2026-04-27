6287f5e01614ab150f0257280aed2360ed9a3c94
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-04-27 00:39:30 -0700

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.js
@@ -0,0 +1,50 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        let recursion1 = (l, r) => {
+            if (l>r) {
+                return -1
+            }
+            let middle = l + Math.floor((r - l) / 2)
+            let currArray = matrix[middle]
+            let innerLeft = currArray[0]
+            let innerRight = currArray[currArray.length - 1]
+            console.log(l,middle,r)
+            if(innerLeft <= target && target <= innerRight) {
+                return middle
+            } else if (target > innerRight) {
+                return recursion1(middle + 1, r)
+            } else {
+                return recursion1(l, middle - 1)
+            }
+
+        }
+        let indexOfArray = recursion1(0, matrix.length - 1)
+        console.log(indexOfArray)
+        if(indexOfArray == -1) {
+            return false
+        }
+        let chosenArray = matrix[indexOfArray]
+
+        let recursion2 = (l, r, arr) => {
+            if (l>r) {
+                return -1
+            }
+            let middle = l + Math.floor((r - l) / 2)
+            let curr = arr[middle]
+            if(curr == target) {
+                return middle
+            } else if (target > curr) {
+                return recursion2(middle + 1, r, arr)
+            } else {
+                return recursion2(l, middle - 1, arr)
+            }
+        }
+        let targetIndex = recursion2(0, chosenArray.length - 1, chosenArray)
+        return targetIndex == -1 ? false : true
+    }
+}
