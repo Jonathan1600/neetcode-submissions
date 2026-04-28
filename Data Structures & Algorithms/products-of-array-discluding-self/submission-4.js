f11fe87508a9936bc79552be68bdea41b91b576f
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-04-27 19:27:35 -0700

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let response = []
+        let product = 1
+        for (let i = 0; i < nums.length; i++) {
+            response[i] = product
+            product *= nums[i]
+        }
+        product = 1
+        for (let i = nums.length - 1; i >= 0; i--) {
+            response[i] *= product
+            product *= nums[i]
+        }
+        return response
+    }
+}
