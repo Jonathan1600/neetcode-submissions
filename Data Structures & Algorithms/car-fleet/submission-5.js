f9cafceb6bb67456294737380472d4f6729c0524
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-04-28 20:50:45 -0700

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        let pxs = {}
+        let stack = []
+        let lastFleetEta = 0
+        let res = 0
+        for(let i = 0; i< position.length; i++) {
+            pxs[position[i]] = speed[i]
+        }
+        position.sort((a,b) => a-b)
+        for(let i = 0; i< position.length; i++) {
+            let currP = position[i]
+            let currS = pxs[position[i]]
+            let eta = (target - currP) / currS
+            stack.push(eta)
+        }
+        while(stack.length > 0) {
+            let popped = stack.pop()
+            if (popped > lastFleetEta) {
+                res++
+                lastFleetEta = popped
+            }
+        }
+        return res
+    }
+}
