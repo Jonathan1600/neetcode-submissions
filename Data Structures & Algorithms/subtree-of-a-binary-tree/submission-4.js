6ae798598ce0e5b58a9f3e3f53cd4c2b12321fb2
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-04-29 10:31:12 -0700

    Add: subtree-of-a-binary-tree - submission-4

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-4.js
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} subRoot
+     * @return {boolean}
+     */
+    isSubtree(root, subRoot) {
+        let isSubTree = false
+        let recursiveCheck = (main, sub) => {
+            console.log(main, sub)
+            if(!main && !sub) {
+                console.log('&&')
+                return true
+            }
+            if(!main || !sub) {
+                if(main && main.val == subRoot.val) {
+                    return recursiveCheck(main, subRoot)
+                } else {
+                    return false
+                }
+            }
+            if (main.val == sub.val) {
+                console.log('--', main.val, sub.val)
+                return recursiveCheck(main.left, sub.left) && recursiveCheck(main.right, sub.right)
+            } else {
+                return false
+            }
+        }
+        let recursion = (node) => {
+            if(!node) {
+                return
+            }
+            if (node.val == subRoot.val) {
+                console.log(node, subRoot, !!isSubTree)
+                isSubTree =  !!isSubTree ? isSubTree : recursiveCheck(node, subRoot)
+                return
+            }
+            recursion(node.left)
+            recursion(node.right)
+        }
+        recursion(root)
+        console.log(isSubTree)
+        return isSubTree
+    }
+}
