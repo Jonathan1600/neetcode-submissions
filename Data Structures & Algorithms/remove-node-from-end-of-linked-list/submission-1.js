1f4447f19148e376a0cecc8423f8ec5ba9e95c9a
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-04-29 09:52:14 -0700

    Add: remove-node-from-end-of-linked-list - submission-1

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @param {number} n
+     * @return {ListNode}
+     */
+    removeNthFromEnd(head, n) {
+        let dummy = {val: 0, next: null}
+        let length = 0
+        let travel = 0
+        let prev = dummy
+        dummy.next = head
+        let recursion = (head) => {
+            if(!head) {
+                return
+            }
+            length++
+            recursion(head.next)
+        }
+        let recursion2 = (head) => {
+            if(!head) {
+                return
+            }
+            if (travel == (length - n)) {
+                prev.next = head.next
+                return
+            } else {
+                prev = head
+            }
+            travel++
+            recursion2(head.next)
+        }
+
+        recursion(head)
+        recursion2(head)
+        return dummy.next
+    }
+}
