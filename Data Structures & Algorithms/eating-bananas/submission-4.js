072b700fa076b9561f8ed788c86e9b6ddcea6385
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-04-28 19:57:56 -0700

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let mn = 0
+        let mx = 0
+        let minNumber = Infinity
+        for (let i = 0; i<piles.length;i++) {
+            let num = piles[i]
+            mn = Math.min(num, mn)
+            mx = Math.max(num, mx)
+        }
+
+        let recursion = (left,right) => {
+            if(left > right) {
+                return
+            }
+            let mid = left + Math.floor((right-left) / 2)
+            let totalTime = 0
+            for (let i = 0; i<piles.length;i++) {
+                if(totalTime > h) {
+                    break
+                }
+                let num = piles[i]
+                let timeToEat = Math.ceil(num / mid)
+                totalTime += timeToEat
+            }
+            if(totalTime <= h) {
+                minNumber = Math.min(minNumber,mid)
+            }
+            if(totalTime > h) {
+                recursion(mid + 1, right)
+            } else {
+                recursion(left, mid - 1)
+            }
+        }
+        recursion(mn, mx)
+        return minNumber
+    }
+}
