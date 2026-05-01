5716953df0bbface7fd8b4fe34b380b457d298a6
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-05-01 15:12:37 -0700

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const nHashmap = {} 
+
+        for (let i=0; i<nums.length;i++) {
+            if ((target - nums[i]) in nHashmap) {
+                return [i, nHashmap[target-nums[i]]]
+            }
+            nHashmap[nums[i]] = i
+        }
+        return []
+    }
+}
