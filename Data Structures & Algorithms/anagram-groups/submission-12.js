45f22ef57298cd2fc919df2b6ad4d5442e487e29
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-05-01 15:37:19 -0700

    Add: anagram-groups - submission-12

---
+++ Data Structures & Algorithms/anagram-groups/submission-12.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let frequency = {}
+        for (let i = 0; i < strs.length;i++) {
+            let holder = new Array(27).fill(0)
+            for(const char in strs[i]) {
+                let character = strs[i][char]
+                let alphabetNumber = character.charCodeAt(0) - 96
+                holder[alphabetNumber] = holder[alphabetNumber] + 1
+
+            }
+            let key = holder.join(',')
+            if (!(key in frequency)) {
+                frequency[key] = []
+            }
+            frequency[key].push(strs[i])
+        }
+        return Object.values(frequency)
+    }
+}
