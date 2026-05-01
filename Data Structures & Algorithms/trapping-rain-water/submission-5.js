82e8daea6cdbee2fa3eaaf5a44cbb5e6799501bd
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-04-30 20:59:04 -0700

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let maxL = 0
+        let maxR = 0
+        let l = 0
+        let r = height.length - 1 
+        let maxWater = 0
+        while(l < r) {
+            let lh = height[l]
+            let rh = height[r]
+            console.log("---", lh, maxL, r, maxR)
+            if(lh > rh) {
+                let water = Math.min(maxR, maxL) - rh 
+                maxWater += water > 0 ? water : 0
+                maxL = Math.max(maxL, lh)
+                r--
+            } else {
+                let water = Math.min(maxR, maxL) - lh
+                maxWater += water > 0 ? water : 0
+                maxR = Math.max(maxR, rh)
+                l++
+            }
+        }
+        return maxWater
+
+    }
+}
