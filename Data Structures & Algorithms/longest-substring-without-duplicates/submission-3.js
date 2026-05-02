d5201eaf176f5500bb2d6bc6390d42dce24b8be5
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-05-01 19:33:15 -0700

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        if (s.length == 0) return 0
+        const seen = new Set()
+        let l = 0
+        let max = 0
+        for (let r = 0; r < s.length; r++) {
+            let char = s[r]
+            if(seen.has(char)) {
+                while(seen.has(s[l])) {
+                    seen.delete(s[l])
+                    l++
+                }
+            }
+            max = Math.max(max, (r - l)+1)
+            seen.add(char)
+        }
+        return max
+    }
+}
