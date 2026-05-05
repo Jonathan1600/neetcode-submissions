0da39fdf8694103cb4efc12ab8c7573ff4986c46
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-05-04 23:36:36 -0700

    Add: find-minimum-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums) {
+        let recursion = (l, r) => {
+            let lNum = nums[l]
+            let rNum = nums[r]
+            console.log("START--","L:",l,lNum , "R:",r, rNum)
+            let m = l + Math.floor((r - l)/2)
+            let mNum = nums[m]
+            console.log("M--",m, mNum)
+            if(l == 0 && lNum < rNum) {
+                return lNum
+            }
+            if(l == r) {
+                return lNum
+            }
+            if(mNum < nums[m - 1]) {
+                return mNum
+            }
+            if(mNum >= lNum && mNum >= rNum) {
+                return recursion(m + 1, r)
+            } else {
+                return recursion(l, m - 1)
+            }
+        }
+        return recursion(0, nums.length - 1)
+    }
+}
