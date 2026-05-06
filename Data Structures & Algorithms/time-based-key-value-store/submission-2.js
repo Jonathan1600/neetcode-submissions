5fce75ea246667ff27598189b8c07c5427eb3161
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-05-05 23:07:32 -0700

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.js
@@ -0,0 +1,43 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        let keyStore = this.keyStore
+        let node = {val: value, timestamp: timestamp, next: null}
+        if (keyStore.has(key)) {
+            let head = keyStore.get(key)
+            while(head.next) {
+                head = head.next
+            }
+            head.next = node
+        } else {
+            keyStore.set(key, node)
+        }
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        let keyStore = this.keyStore
+        if (!keyStore.has(key)) return ''
+        let head = keyStore.get(key)
+        while (head) {
+            if(head.timestamp <= timestamp && !(head.next && head.next.timestamp <= timestamp)) {
+                return head.val
+            }
+            head = head.next
+        }
+        return ''
+    }
+}
