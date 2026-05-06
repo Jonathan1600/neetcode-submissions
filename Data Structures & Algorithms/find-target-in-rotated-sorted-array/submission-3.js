77a1da0340934a70d36e8959ad66c64880715237
Jonathan Calderon <jonathancalderonsilberman@gmail.com>
2026-05-05 19:45:37 -0700

    Add: find-target-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-3.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let recursion = (l, r) => {
+            console.log(l, r)
+            if(l >= r) {
+                return nums[l] == target ? l : -1
+            }
+            let m = l + Math.floor((r-l)/2)
+            if (nums[m] == target) {
+                return m
+            }
+            if(nums[m] > nums[r]) {
+                if (nums[m] >= nums[0] && (target < nums[r] || target > nums[m])) {
+                    return recursion(m + 1, r)
+                } else {
+                    return recursion(l, m - 1)
+                }
+            } else {
+                if (target > nums[m]) {
+                    return recursion(m + 1, r)
+                } else {
+                    return recursion(l, m - 1)
+                }
+            }
+        }
+        return recursion(0, nums.length -1)
+    }
+}
